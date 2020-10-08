e7c6db078e78c8712f4e46af7a0ab03064a54838
Thabiso Shabangu <thabisos@atticoza.onmicrosoft.com>
2020-10-08 16:02:42 +0200

    About Page UI Complete

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,12 +1,78 @@
 <template>
-  <div class="home">
-      <h1>About </h1>
-    <img alt="Vue logo" src="../../assets/logo.png">
-  </div>
+	<div class="container-lg">
+		<div class="row py-4 mb-4">
+			<div class="col-lg-8">
+				<h1 class="h1">About ATTI Nelspruit</h1>
+				<p class="my-3">
+					ATTI Nelspruit is proud to assist you with the intention of equipping learners with the necessary knowledge and skills to make them computer literate, competent and productive in the workplace. Classes are kept relatively small to enable learners to receive individual attention.
+				</p>
+				<p class="my-3 lh-base]">
+					Within South Africa, ATTI is committed to aligning itself to the national initiatives regarding training and development strategies in the country. We are committed to delivering outcomes and modular based training and in addition, have made representations with the appropriate industry organisations to ensure fulfilling national standards set out by MICT SETA
+				</p>
+				<p class="my-3">
+					ATTI Nelspruit is viewed as a SME with BEE grounding. You can be confident about obtaining training that meets both the local and international standards through ATTI’s training and education centres.
+				</p>
+				<p class="my-3">
+					Or primary training sector is within the ICT.
+				</p>
+			</div>
+			<div class="col-lg-4 py-2">
+				<img class="img-fluid" src="@/assets/images/advertising.png" alt="" srcset="">
+			</div>
+
+			<div class="col-lg-8 mt-4">
+				<h2 class="h2 mb-3">Our Mission and Vision</h2>
+				<p class="my-3">
+					The mission of ATTI Nelspruit College, a Private Further Education and Training College, is to provide the highest quality education in the Information and Communications Technology Sector. We aim to make a difference in and around our province focusing on Cutting Edge Qualifications, Quality Staff, Up to date Curricula and Leading by Example. We seek to play a key-role in the directions and deployment of technology within our respective areas.
+				</p>
+				<p class="my-3">
+					To provide education of the highest quality with these emphases, ATTI’s mission requires selection of an outstanding and caring faculty committed to creating an open, cooperative, learning environment. The College provides for the continuous support and development of this faculty. We aim to grow in the recognition and respect we represent through pioneering and leading in the effective deployment of technology and know-how.
+				</p>
+				<p class="my-3">
+					The teaching-learning process at ATTI is shaped by a view of education as a process of awakening the “learner within,” so that our students will become lifelong learners. Students are encouraged to be active, involved members of society. We will foster career building by ensuring organisational pride, loyalty and emotional ownership of oneself. Instilling the highest standard of Business Ethics.
+				</p>
+				<p class="my-3">
+					The education is carried on with a concern for the world in which we live and for improving human society. We strive to educate morally sensitive leaders for future generations. Therefore ATTI stresses global education, peaceful resolution of conflict, equality of individuals, and high moral standards of personal conduct.
+				</p>
+				<p class="mt-3 mb-2 font-weight-bold">
+					In fulfilling its mission, ATTI adopts the following goals:
+				</p>
+				<ul>
+					<li class="my-2">
+						To attract outstanding students from diverse backgrounds
+					</li>
+					<li class="my-2">
+						To create a market awareness within the ICT Sector
+					</li>
+					<li class="my-2">
+						To develop a diverse faculty which is nationally and internationally recognized for excellence in teaching
+					</li>
+					<li class="my-2">
+						To provide a challenging program with an ICT curriculum that encourages creativity, independent thought, and intellectual depth, breadth, and curiosity
+					</li>
+					<li class="my-2">
+						To offer high-quality graduate and professional skills programs that prepare students for intellectual, professional, and public leadership
+					</li>
+					<li class="my-2">
+						To instill in its students an appreciation for the human condition, a concern for the public well-being, and a life-long commitment to learning
+					</li>
+				</ul>
+			</div>
+
+			<div class="col-lg-4 py-2">
+				<img class="img-fluid" src="@/assets/images/technicalsupport.png" alt="" srcset="">
+			</div>
+		</div>
+	</div>
+
+	<!-- Add Footer Componets here -->
+    <Footer />
 </template>
 
 <script>
+import Footer from '@/components/home/Footer.vue'
 export default {
+	components: { Footer },
     setup() {
 
     }
